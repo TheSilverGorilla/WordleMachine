8e787617af004628527cb67dd097dee5e9283fbb
TheSilverGorilla <83459890+TheSilverGorilla@users.noreply.github.com>
2023-07-21 11:23:20 -0500

    Update select.pl
    Added basic input

--- select.pl
+++ select.pl
@@ -1,6 +1,11 @@
 fitb([],[]).
 fitb([H|T], [H|T1]) :- fitb(T,T1).
-fitb([H|T], [_|T1]) :- H='.', fitb(T,T1).
+fitb([H|T], [_|T1]) :- H='.' , fitb(T,T1).
 pwordle(_,[],_).
 pwordle(P, [H | _], H) :- atom_chars(H, Characters), fitb(P, Characters).
 pwordle(P, [_| Rest], W) :- pwordle(P, Rest, W).
+
+wordle(L) :- write("Enter search term"), read(A), process(A, L).
+
+process(stop,_) :- !.
+process(A, L) :- atom_chars(A,T),pwordle(T,L,C), write(C), nl, wordle(L).
